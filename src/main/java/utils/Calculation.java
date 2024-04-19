3f46753b0f212a3fa16fbf12039b2a4a192f2aae
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-04-19 23:57:21 +0200

    Move Calculation classes to utils package
    The Calculation class and its corresponding test class have been moved to a new 'utils' package. This is to maintain the code's organization and make the project structure cleaner. The import statements in Main.java have been adjusted accordingly.

--- src/main/java/utils/Calculation.java
+++ src/main/java/utils/Calculation.java
@@ -1,3 +1,5 @@
+package utils;
+
 public class Calculation {
     public static int sum(int a, int b) {
         return a + b;
--- src/test/java/utils/CalculationTest.java
+++ src/test/java/utils/CalculationTest.java
@@ -1,4 +1,7 @@
+package utils;
+
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculationTest {
