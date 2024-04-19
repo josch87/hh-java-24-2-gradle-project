6d1fcf252d7b5d199140e4c2dcafa8334f34e5ca
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-04-19 23:41:34 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Gradle Project started");
+
+        int a = 127;
+        int b = 128;
+        int sum = Calculation.sum(a,b);
+
+        System.out.println(sum);
+    }
+}
---
+++ src/test/java/CalculationTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculationTest {
+
+    @Test
+    public void sum_WhenBothNumbersArePositive_ThenReturnSum() {
+        // GIVEN
+        int a = 6;
+        int b = 7;
+
+        // WHEN
+        int actual = Calculation.sum(a, b);
+
+        // THEN
+        int expected = 13;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sum_WhenBothNumbersAreNegative_ThenReturnSum() {
+        // GIVEN
+        int a = -4;
+        int b = -3;
+        
+        // WHEN
+        int actual = Calculation.sum(a, b);
+
+        // THEN
+        int expected = -7;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sum_WhenOneNumberIsZero_ThenReturnSum() {
+        // GIVEN
+        int a = 0;
+        int b = 5;
+        
+        // WHEN
+        int actual = Calculation.sum(a, b);
+
+        // THEN
+        int expected = 5;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sum_WhenBothNumbersAreZero_ThenReturnSum() {
+        // GIVEN
+        int a = 0;
+        int b = 0;
+        
+        // WHEN
+        int actual = Calculation.sum(a, b);
+
+        // THEN
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
